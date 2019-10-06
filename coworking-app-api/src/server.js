5dfeff009e6a162bb647f4b8f57dc476d88be452
willianfalbo <willianfalbo@gmail.com>
2019-10-06 14:00:49 -0300

    create backend api using Node.js and Express.js

--- coworking-app-api/src/server.js
+++ coworking-app-api/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -14,8 +16,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-uyaue.mongodb.net/
 // "req.body" to access the body from the request
 // "req.headers" to access the headers from the request
 
+app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
